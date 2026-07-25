5147b46282fd4e7871d6931451b926075885bad1
PrasadKharade <prasad1kharade@gmail.com>
2026-07-25 17:21:09 +0530

    Combine Two Tables

---
+++ python/objects.py
@@ -0,0 +1,17 @@
+class Students:
+    name = "Prasad"
+
+    def __init__(self,name,price): #construct
+        self.name = name
+        self.price = price
+
+
+s1 = Students("Prasad",10000)
+
+
+
+
+
+
+print(s1.name)
+print(s1.price)    
