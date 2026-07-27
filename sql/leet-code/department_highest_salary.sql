3d9f09b6d5709870c9305c860dbe77673bec4524
PrasadKharade <prasad1kharade@gmail.com>
2026-07-27 12:27:40 +0530

    Solved department-highest-salary

---
+++ sql/leet-code/department_highest_salary.sql
@@ -0,0 +1,12 @@
+-- link: https://leetcode.com/problems/department-highest-salary/
+
+/* Write your T-SQL query statement below */
+select Department, Employee,Salary from
+(
+select  a.name as Employee, a.salary as Salary, b.name as Department,
+DENSE_RANK()over(partition by b.name order by a.salary desc)  as Rn
+from Employee a 
+left join Department b 
+on a.departmentId = b.id
+)A
+where Rn = 1
