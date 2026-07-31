e50d87a15544acb0a84ba55e02941371f7876b9a
PrasadKharade <prasad1kharade@gmail.com>
2026-07-31 23:14:02 +0530

    Solved big-countries

---
+++ sql/leet-code/big-countries.sql
@@ -0,0 +1,4 @@
+-- link: https://leetcode.com/problems/big-countries/
+
+/* Write your T-SQL query statement below */
+select name, population, area from World where population>= 25000000 OR area>=3000000
