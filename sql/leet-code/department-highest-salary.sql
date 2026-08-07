fcf9de82097a4586cb0c11303d6b0cbce5f474a1
PrasadKharade <prasad1kharade@gmail.com>
2026-08-07 23:04:42 +0530

    indentation

--- sql/leet-code/department-highest-salary.sql
+++ sql/leet-code/department-highest-salary.sql
@@ -3,10 +3,10 @@
 /* Write your T-SQL query statement below */
 select Department, Employee,Salary from
 (
-select  a.name as Employee, a.salary as Salary, b.name as Department,
-DENSE_RANK()over(partition by b.name order by a.salary desc)  as Rn
-from Employee a 
-left join Department b 
-on a.departmentId = b.id
+    select  a.name as Employee, a.salary as Salary, b.name as Department,
+    DENSE_RANK()over(partition by b.name order by a.salary desc)  as Rn
+    from Employee a 
+    left join Department b 
+    on a.departmentId = b.id
 )A
 where Rn = 1
