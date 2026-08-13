603d9a0f64cd39c33be255a2293970f826f7339c
PrasadKharade <prasad1kharade@gmail.com>
2026-08-13 11:52:22 +0530

    Solved palindrome using 2 pointers technique

--- python/python-dsa/phase-0/palindrome.py
+++ python/python-dsa/phase-0/palindrome.py
@@ -3,19 +3,19 @@
 #Approach 1 
 # Brute Force approach
 
-s = "madam1"
-left_right = ""
-right_left = ""
-for i in range(len(s)-1,-1,-1):
-    right_left = right_left + s[i]
+# s = "madam"
+# left_right = ""
+# right_left = ""
+# for i in range(len(s)-1,-1,-1):
+#     right_left = right_left + s[i]
 
-# for i in range(len(s)):
-#     left_right = left_right + s[i]    
+# # for i in range(len(s)):
+# #     left_right = left_right + s[i]    
     
-if right_left == s:
-    print("String is palindrome")
-else:
-    print("String is not a palindrome")
+# if right_left == s:
+#     print("String is palindrome")
+# else:
+#     print("String is not a palindrome")
 
 # TC: O(n^2) 
 # SC: O(n)   
@@ -25,3 +25,31 @@ else:
 
 #Approach 2
 # Two pointers technique
+
+s = "madam"
+
+left_max = 0
+right_max = len(s)-1
+
+while left_max < right_max:
+
+    if s[left_max] != s[right_max]:
+        print("String is not a palindrome")
+        break
+
+    left_max += 1
+    right_max -= 1
+
+else:
+    print("String is a palindrome")
+
+    
+
+
+
+
+
+
+
+
+
