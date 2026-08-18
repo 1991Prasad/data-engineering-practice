bbe3bc212830e8795d7af9c041f1179b2521a630
PrasadKharade <prasad1kharade@gmail.com>
2026-08-18 20:07:54 +0530

    Update Move zeros

--- python/python-dsa/phase-0/move-zeros.py
+++ python/python-dsa/phase-0/move-zeros.py
@@ -1,16 +1,16 @@
-nums = [0, 1, 2, 0, 3, 9, 81 , 0]
+nums = [0, 0, 0, 0, 1, 1, 2, 0, 3, 9, 81 ,0]
 
 current = 0
 positional = 0
 
-while positional < len(nums):
+while current < len(nums):
 
-    if nums[positional] != 0:
+    if nums[current] != 0:
         nums[current],nums[positional] = nums[positional],nums[current]
-        current += 1
+        positional += 1
 
 
-    positional += 1 
+    current += 1 
 
     
 
