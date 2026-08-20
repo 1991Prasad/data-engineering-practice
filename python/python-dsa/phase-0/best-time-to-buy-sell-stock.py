a0df2f72bf726091b0ce2b986639a1696bbe045e
PrasadKharade <prasad1kharade@gmail.com>
2026-08-20 11:01:06 +0530

    Solved best time to buy sell stocks

---
+++ python/python-dsa/phase-0/best-time-to-buy-sell-stock.py
@@ -0,0 +1,19 @@
+
+# # Brute Force Approach
+# prices = [7, 1, 5, 3, 6, 4, 10]
+
+# max_profit = 0
+
+# for i in range(len(prices)):
+#     for j in range(i+1,len(prices)):
+#         if prices[j] - prices[i] > max_profit:
+#             max_profit = prices[j] - prices[i]
+
+# print(max_profit)
+
+# # TC: O(n^2)
+# # SC: O(1)
+
+
+
+
